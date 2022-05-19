a2b921cc6a13dbd1e1ee69045e261a0d6444dcb3
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-18 20:39:10 -0500

    Solution
    Added Classes

---
+++ Mohamed_Sahal/City.java
@@ -0,0 +1,20 @@
+package Mohamed_Sahal;
+
+public class City {
+
+    String City;
+    int Miles;
+
+    public City(String city, int miles) {
+        City = city;
+        Miles = miles;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public int getMiles() {
+        return Miles;
+    }
+}
---
+++ Mohamed_Sahal/Runner/Node.java
@@ -0,0 +1,18 @@
+package Mohamed_Sahal.Runner;
+
+import Mohamed_Sahal.City;
+
+public class Node {
+
+    City city;
+    Node left;
+    Node right;
+
+    public Node(City city) {
+        this.city = city;
+        this.left = left;
+        this.right = right;
+    }
+
+
+}
