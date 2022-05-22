eafb9c9e33338561bb79b8d2b04312050edba365
Mohamed Abdullahi <77897855+Moha-dotcom@users.noreply.github.com>
2022-05-21 23:57:38 -0500

    Adding Solution To Binary Tree Problem

---
+++ Mohamed_Sahal/BinaryTree/Node.java
@@ -0,0 +1,14 @@
+public class Node {
+
+    public City city;
+    public Node left;
+    public Node right;
+
+    public Node(City city) {
+        this.city = city;
+        this.left = left;
+        this.right = right;
+    }
+
+
+}
