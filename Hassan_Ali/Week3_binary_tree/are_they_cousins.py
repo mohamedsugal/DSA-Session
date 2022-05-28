174d2c09d0bee6727b7b9b3e135d5382c26eba23
Hassan_Ali <moha813@gmail.com>
2022-05-28 17:24:15 -0500

    removed repetitive variable declarations and initializations

--- Hassan_Ali/Week3_binary_tree/are_they_cousins.py
+++ Hassan_Ali/Week3_binary_tree/are_they_cousins.py
@@ -4,80 +4,82 @@ class TreeNode:
         self.left = None
         self.right = None
 
-#using recursion
 
-def are_they_cousins(root:TreeNode,person1:str,person2:str)->bool:
+class Solution:
+   #using recursion
+    def are_they_cousins(self,root:TreeNode,person1:str,person2:str)->bool:
 
-    p1_parent = None
-    p1_depth = 0
-    p1_found = False
-
-    p2_parent = None
-    p2_depth = 0
-    p2_found = False
-
-    # using a helper function to keep track of the parent and depth of the tree
-
-    def find_cousins(node:TreeNode,parent:TreeNode,depth:int)->bool:
-        if not node:
-            return False
-
-        if person1 == node.val:
-           nonlocal p1_found 
-           p1_found = True
-           nonlocal p1_depth 
-           p1_depth = depth
-           nonlocal p1_parent
-           p1_parent = parent
-
-        if person2 == node.val:
-            nonlocal p2_found
-            p2_found = True
-            nonlocal p2_depth
-            p2_depth = depth
-            nonlocal p2_parent
-            p2_parent = parent
-
+        self.p1_parent = None
         
-        find_cousins(node.left,node,depth+1)
-        find_cousins(node.right,node,depth+1)
+        self.p1_depth = 0
+        self.p1_found = False
 
-    find_cousins(root,None,0)
-    if p1_parent != p2_parent and p1_depth == p2_depth and p1_found and p2_found:
-        return True
-    return False
+        self.p2_parent = None
+        self.p2_depth = 0
+        self.p2_found = False
+
+        # using a helper function to keep track of the parent and depth of the tree
+
+        def find_cousins(node:TreeNode,parent:TreeNode,depth:int)->bool:
+            if not node:
+                return False
+
+            if person1 == node.val:
+            
+                self.p1_found = True
+                
+                self.p1_depth = depth
+                
+                self.p1_parent = parent
+
+            if person2 == node.val:
+            
+                self.p2_found = True
+            
+                self.p2_depth = depth
+            
+                self.p2_parent = parent
+
+            
+            find_cousins(node.left,node,depth+1)
+            find_cousins(node.right,node,depth+1)
+
+        find_cousins(root,None,0)
+        if self.p1_parent != self.p2_parent and self.p1_depth == self.p2_depth and self.p1_found and self.p2_found:
+            return True
+        return False
 
 
 
 
-# using iterative
+    # using iteration
 
-# def are_they_cousins(root,person1,person2):
+    # def are_they_cousins(root,person1,person2):
 
-#     if not root:
-#         return False
-#     stack = [(root,None,0)]
+    #     if not root:
+    #         return False
+    #     stack = [(root,None,0)]
 
-#     while stack:
-#         node,parent,depth = stack.pop()
+    #     while stack:
+    #         node,parent,depth = stack.pop()
 
-#         if person1 == node.val:
-#             p1_found = True
-#             p1_depth = depth
-#             p1_parent = parent
-#         if person2 == node.val:
-#             p2_found = True
-#             p2_depth = depth
-#             p2_parent = parent
+    #         if person1 == node.val:
+    #             p1_found = True
+    #             p1_depth = depth
+    #             p1_parent = parent
+    #         if person2 == node.val:
+    #             p2_found = True
+    #             p2_depth = depth
+    #             p2_parent = parent
 
-#         if node.right:
-#             stack.append((node.right,node,depth+1))
-#         if node.left:
-#             stack.append((node.left,node,depth+1))
+    #         if node.right:
+    #             stack.append((node.right,node,depth+1))
+    #         if node.left:
+    #             stack.append((node.left,node,depth+1))
 
-#     if p1_parent != p2_parent and p1_depth == p2_depth and p1_found and p2_found:
-#         return True
-#     return False
+    #     if p1_parent != p2_parent and p1_depth == p2_depth and p1_found and p2_found:
+    #         return True
+    #     return False
 
 
 tree = TreeNode("Omar")
@@ -86,6 +88,7 @@ tree.right = TreeNode("Gedi")
 tree.left.left = TreeNode("Abdi")
 tree.right.right = TreeNode("Nimo")
 
-print(are_they_cousins(tree,"Abdi","Nimo"))
+sol = Solution()
+print(sol.are_they_cousins(tree,"Gedi","Jama"))
 
        
